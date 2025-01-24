6218090b59b318b9ef21913f8c11f4a368a073ad
Olena-Koval <19111982e@gmail.com>
2025-01-24 13:38:32 +0100

    eliminado .eslintrc.json, cambiado eslint.config.mjs, cambiado script.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      quotes: ["error", "double"]
+    }
+},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
