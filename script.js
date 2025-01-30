8863b8fd87b9c3c574f0b564c6bf5175cd56bb10
Olena-Koval <19111982e@gmail.com>
2025-01-30 12:57:20 +0100

    cambios en index y script

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const INITIAL_SCORE = 20
 const MAX_NUMBER = 20
 
-initData()
-function initData() {
-  let score = INITIAL_SCORE
-  let highscore = 0
-  let number = Math.trunc(Math.random() * MAX_NUMBER) + 1
-}
+let number, highscore, score
 
 /* seleccionar todos los elementos del DOM que necesitamos */
 const messageField = document.querySelector('.message')
@@ -17,10 +12,85 @@ const guessField = document.querySelector('.guess')
 const checkBtn = document.querySelector('.check')
 const againBtn = document.querySelector('.again')
 
+// el estado de mi aplicación se basa en:
+// -number: número aleatorio
+// score
+// highscore
+// si uno de estos cambia, se actualiza el DOM a posteriori
+
+initData()
+
+function initData() {
+  score = INITIAL_SCORE
+  scoreField.textContent = score
+  highscore = Number(localStorage.getItem('highscore')) || 0
+  highscoreField.textContent = highscore
+  highscore = highscore || 0
+  number = Math.trunc(Math.random() * MAX_NUMBER) + 1
+  console.log(number, 'el número secreto')
+}
+
 checkBtn.addEventListener('click', checkNumber)
+
+againBtn.addEventListener('click', playAgain)
+
+// si pulsamos la tecla ENTER debería pulsar el botón checkButton
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    checkNumber()
+  }
+})
+
+function playAgain() {
+  initData()
+  numberField.textContent = '?'
+  numberField.style.width = '15rem'
+  numberField.style.fontSize = '6rem'
+  numberField.style.backgroundColor = 'white'
+  document.body.style.backgroundColor = '#222'
+  checkBtn.disabled = false
+  // actualizo el campo highscore y highscoreField con el localStorage
+  // si no hay highscore en el localStorage, highscore es 0
+  // highscore tiene que ser number!!!
+  highscore = Number(localStorage.getItem('highscore')) || 0
+  highscoreField.textContent = highscore
+}
+
 function checkNumber() {
-  //obtenemos el numero pulsado
-  //si no es un numero que lo correcta
-  //si es un numero y no es correcro... -> comprobamos score: perdemos partida? o actualizamos nuestras variables y el DOM
-  //si es un numero y es correcto ... -> comprobamos score: ganamos partida? o actualizamos nuestras variables y el DOM
+  // obtenemos el número pulsado
+  const guess = Number(guessField.value)
+
+  // si no es un número que lo corrija y tiene que estar entre 1 y 20
+  if (!guess || guess < 1 || guess > 20) {
+    displayMessage('😢 Introduce un número entre 1 y 20')
+  } else if (guess === number) {
+    displayMessage('🎉 Número correcto!')
+    numberField.textContent = number
+    numberField.style.width = '30rem'
+    numberField.style.fontSize = '10rem'
+    numberField.style.backgroundColor = 'red'
+    document.body.style.backgroundColor = 'green'
+    checkBtn.disabled = true
+    if (score > highscore) {
+      highscore = score
+      highscoreField.textContent = highscore
+      // guardamos el valor en el localStorage
+      localStorage.setItem('highscore', highscore)
+    }
+  } else {
+    if (score > 1) {
+      const message =
+        guess > number ? '😢 Te has pasado!' : '😢 Te has quedado corto!'
+      displayMessage(message)
+    } else {
+      displayMessage('😢 Perdiste!')
+      checkBtn.disabled = true
+    }
+    score--
+    scoreField.textContent = score
+  }
+}
+
+function displayMessage(message) {
+  messageField.textContent = message
 }
